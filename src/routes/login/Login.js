586c44d15ede534c6936916ca2ecfd29c10e2e83
Tran Anh Tuan <tk1cntt@gmail.com>
2018-12-05 21:06:26 +0700

    Add google fb login

--- src/routes/login/Login.js
+++ src/routes/login/Login.js
@@ -9,6 +9,8 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
+import GoogleLogin from 'react-google-login';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Login.css';
 
@@ -17,16 +19,31 @@ class Login extends React.Component {
     title: PropTypes.string.isRequired,
   };
 
-  render() {
-    return (
-      <div className={s.root}>
-        <div className={s.container}>
-          <h1>{this.props.title}</h1>
-          <p className={s.lead}>
-            Log in with your username or company email address.
-          </p>
-          <div className={s.formGroup}>
-            <a className={s.facebook} href="/login/facebook">
+  responseFacebook = response => {
+    console.log(response); // eslint-disable-line
+  };
+
+  responseGoogle = response => {
+    console.log(response); // eslint-disable-line
+  };
+
+  facebookLoginDom() {
+    const facebookLogin =
+      typeof window !== 'undefined' ? (
+        <FacebookLogin
+          appId="504517876723313"
+          autoLoad
+          fields="name,email,picture"
+          scope="public_profile,email"
+          callback={this.responseFacebook}
+          render={renderProps => (
+            <div
+              onClick={renderProps.onClick}
+              onKeyPress={() => {}}
+              tabIndex={0}
+              role="button"
+              className={s.facebook}
+            >
               <svg
                 className={s.icon}
                 width="30"
@@ -37,10 +54,31 @@ class Login extends React.Component {
                 <path d="M22 16l1-5h-5V7c0-1.544.784-2 3-2h2V0h-4c-4.072 0-7 2.435-7 7v4H7v5h5v14h6V16h4z" />
               </svg>
               <span>Log in with Facebook</span>
-            </a>
-          </div>
-          <div className={s.formGroup}>
-            <a className={s.google} href="/login/google">
+            </div>
+          )}
+        />
+      ) : (
+        undefined
+      );
+    return facebookLogin;
+  }
+
+  googleLoginDom() {
+    const googleLogin =
+      typeof window !== 'undefined' ? (
+        <GoogleLogin
+          clientId="492545033226-oscl26h1lmomsg25u8fr7ov8m8kjukn9.apps.googleusercontent.com"
+          buttonText="Log in with Google"
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+          render={renderProps => (
+            <div
+              onClick={renderProps.onClick}
+              onKeyPress={() => {}}
+              tabIndex={0}
+              role="button"
+              className={s.google}
+            >
               <svg
                 className={s.icon}
                 width="30"
@@ -62,32 +100,25 @@ class Login extends React.Component {
                 />
               </svg>
               <span>Log in with Google</span>
-            </a>
-          </div>
-          <div className={s.formGroup}>
-            <a className={s.twitter} href="/login/twitter">
-              <svg
-                className={s.icon}
-                width="30"
-                height="30"
-                viewBox="0 0 30 30"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d={
-                    'M30 6.708c-1.105.49-2.756 1.143-4 1.292 1.273-.762 2.54-2.56 ' +
-                    '3-4-.97.577-2.087 1.355-3.227 1.773L25 5c-1.12-1.197-2.23-2-4-2-3.398 0-6 ' +
-                    '2.602-6 6 0 .4.047.7.11.956L15 10C9 10 5.034 8.724 2 5c-.53.908-1 1.872-1 ' +
-                    '3 0 2.136 1.348 3.894 3 5-1.01-.033-2.17-.542-3-1 0 2.98 4.186 6.432 7 7-1 ' +
-                    '1-4.623.074-5 0 .784 2.447 3.31 3.95 6 4-2.105 1.648-4.647 2.51-7.53 2.51-.5 ' +
-                    '0-.988-.03-1.47-.084C2.723 27.17 6.523 28 10 28c11.322 0 17-8.867 17-17 ' +
-                    '0-.268.008-.736 0-1 1.2-.868 2.172-2.058 3-3.292z'
-                  }
-                />
-              </svg>
-              <span>Log in with Twitter</span>
-            </a>
-          </div>
+            </div>
+          )}
+        />
+      ) : (
+        undefined
+      );
+    return googleLogin;
+  }
+
+  render() {
+    return (
+      <div className={s.root}>
+        <div className={s.container}>
+          <h1>{this.props.title}</h1>
+          <p className={s.lead}>
+            Log in with your username or company email address.
+          </p>
+          <div className={s.formGroup}>{this.facebookLoginDom()}</div>
+          <div className={s.formGroup}>{this.googleLoginDom()}</div>
           <strong className={s.lineThrough}>OR</strong>
           <form method="post">
             <div className={s.formGroup}>
